d23e886b3bcbbbc39597fc8c4ebc723e2ab241dd
HoangDuc39 <nghiaduc3901@gmail.com>
2023-09-13 17:41:44 +0700

    Add Product

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 class AdminController extends Controller
 {
     public function view_category()
@@ -23,5 +24,27 @@ class AdminController extends Controller
         $data = category::find($id);
         $data->delete();
         return redirect()->back()->with('message', 'Category Deleted Successfully');
+    }
+    public function view_product()
+    {
+        $category = category::all();
+        return view('admin.product',compact('category'));
+    }
+    public function add_product(Request $request)
+    {
+        $data =  new product;
+        $data->title = $request->title;
+        $data->description = $request->description;
+        $data->price = $request->price;
+        $data->quantity = $request->quantity;
+        $data->discount_price = $request->discount_price;
+        $data->category = $request->category;
+
+        $image= $request->image;
+        $image_name=time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('product',$image_name);
+        $data->image=$image_name;
+        $data->save();
+        return redirect()->back()->with('message', 'Product Added Successfully');
     }
 }
